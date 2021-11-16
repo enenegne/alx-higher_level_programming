22fd62e2cde20fac0fdcbb18a70221d71378bac0
enenegne <mikiasabiyy@gmail.com>
2021-11-16 15:16:20 -0500

    Task - 5 correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print("{}".format(i))
-        break
-    if i <= 9:
-        print("0{},".format(i), end=" ")
-    else:
-        print("{},".format(i), end=" ")
+print(', '.join(['{:02d},'.format(n)for n in range(99)]))
